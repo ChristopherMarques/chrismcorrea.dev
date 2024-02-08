c601fd0b5cad7a2e9cf0c792aa764f9ff6fb4b91
Christopher Correa <chriscorrea.marques@gmail.com>
2024-02-08 11:58:34 -0300

    fix image border

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
-import Container from '../container'
 
 const Loader = () => {
 	return (
-		<div className='flex justify-center items-center'>
+		<div className='flex justify-center items-center rounded-full border-transparent'>
 			<Image
 				quality={90}
 				src={'/img/loader.svg'}
 				width={500}
 				height={500}
+				className='rounded-full border-transparent'
 				alt='loading'
 			/>
 		</div>
