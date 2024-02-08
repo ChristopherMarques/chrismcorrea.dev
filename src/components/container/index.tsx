0c053a9f5c7a5b23bd7ea43d5e7b4fdcbeca2c65
Christopher Correa <chriscorrea.marques@gmail.com>
2024-02-08 10:43:57 -0300

    update website background

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -6,8 +6,8 @@ interface ContainerProps {
 
 const Container = ({ children }: ContainerProps) => {
 	return (
-		<div className="flex flex-col w-screen h-screen py-1.5 px-3 bg-[url('/img/noise-background.svg')]">
-			<div className="flex flex-col w-full h-full justify-center bg-[url('/img/grid.svg')] bg-center bg-opacity-5 bg-no-repeat object-cover">
+		<div className="flex flex-col w-screen h-screen py-1.5 px-3 bg-[url('/img/noise-background.svg')] object-cover bg-center bg-no-repeat bg-opacity-0">
+			<div className="flex flex-col w-full h-full justify-center bg-[url('/img/grid.svg')] bg-opacity-5 bg-no-repeat bg-top object-cover">
 				{children}
 			</div>
 		</div>
